3485976dfd8fba03bdf30b0d5cf412c8a8e507dc
Tat Dat Duong <david@duong.cz>
2023-01-15 22:51:09 +0100

    Refactor map generation

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,17 +1,29 @@
+const fs = require("fs")
+const path = require("path")
+const prettier = require("prettier")
+
 const digit = Array(10)
   .fill(0)
   .map((_, i) => i)
 
-// console.log(
-//   JSON.stringify(
-//     digit.map((x) => digit.map((y) => [(x + y) % 10, x + y >= 10])),
-//     null,
-//     2
-//   )
-// )
+const addMapCarry = digit.map((x) =>
+  digit.map((y) => [(x + y) % 10, x + y >= 10])
+)
 
-console.log(
-  JSON.stringify(
-    digit.map((x) => digit.map((y) => [(10 + (x - y)) % 10, (x - y) < 0]))
-  )
+const subMapCarry = digit.map((x) =>
+  digit.map((y) => [(10 + (x - y)) % 10, x - y < 0])
+)
+
+const mulMapCarry = digit.map((x) =>
+  digit.map((y) => [(x * y) % 10, Math.floor((x * y) / 10)])
+)
+
+fs.writeFileSync(
+  path.resolve(__dirname, "../src/map.ts"),
+  prettier.format(
+    `export type AddMapCarry = ${JSON.stringify(addMapCarry)}
+    export type SubMapCarry = ${JSON.stringify(subMapCarry)}`,
+    { semi: false, parser: "babel-ts" }
+  ),
+  { encoding: "utf-8" }
 )
