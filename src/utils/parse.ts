37e5a7d483ec2a4ba75bf536e264b390d05955bc
Tat Dat Duong <david@duong.cz>
2023-03-15 23:37:28 +0100

    Fix broken float parsing

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -5,10 +5,11 @@ export type NumberLike = string | number
 export type ParseNumber<T extends NumberLike> =
   `${T}` extends `${infer N extends number}` ? N : never
 
+// TODO: validate if Int and Frac consist of numerical digits
 export type ParseFloat<T extends NumberLike> =
-  `${T}` extends `${infer Int extends number}.${infer Frac extends string}`
+  `${T}` extends `${infer Int extends string}.${infer Frac extends string}`
     ? { int: `${Int}`; frac: Frac }
-    : `${T}` extends `${infer Int extends number}`
+    : `${T}` extends `${infer Int extends string}`
     ? { int: `${Int}`; frac: "" }
     : never
 
