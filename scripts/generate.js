c74ca3c35e617464c2a934d0e4f66aec5ace30e9
Tat Dat Duong <david@duong.cz>
2023-03-16 22:49:45 +0100

    Add generate math

--- scripts/generate.js
+++ scripts/generate.js
@@ -18,11 +18,14 @@ const mulMapCarry = digit.map((x) =>
   digit.map((y) => [(x * y) % 10, Math.floor((x * y) / 10)])
 )
 
+const cmpDigitMap = digit.map((x) => digit.map((y) => Math.sign(x - y)))
+
 fs.writeFileSync(
   path.resolve(__dirname, "../src/utils/map.ts"),
   prettier.format(
     `export type AddMapCarry = ${JSON.stringify(addMapCarry)}
-    export type SubMapCarry = ${JSON.stringify(subMapCarry)}`,
+    export type SubMapCarry = ${JSON.stringify(subMapCarry)}
+    export type CmpMap = ${JSON.stringify(cmpDigitMap)}`,
     { semi: false, parser: "babel-ts" }
   ),
   { encoding: "utf-8" }
