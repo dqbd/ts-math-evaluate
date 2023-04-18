c2feec5fd62efe3fed2b4c5aab01d899b246dae0
Tat Dat Duong <david@duong.cz>
2023-04-18 23:29:15 +0200

    Simplify root calculation

--- src/math/root.ts
+++ src/math/root.ts
@@ -1,8 +1,8 @@
 import { AddSignFloatNumber } from "./add"
-import { DivideInt, DivideSignFloatNumber } from "./divide"
+import { DivideSignFloatNumber } from "./divide"
 import { MultiplySignFloat } from "./multiply"
 import { PowerSignFloatNumbers } from "./power"
-import { SubDigit, SubSignFloatNumber } from "./subtract"
+import { SubSignFloatNumber } from "./subtract"
 import {
   Digit,
   FloatNumber,
@@ -13,61 +13,6 @@ import {
 } from "../utils/parse"
 
 type OneSignFloatNumber = SignFloatNumber<"+", FloatNumber<[1], []>>
-
-type RootNSubOne<N extends Digit[]> = SignFloatNumber<
-  "+",
-  FloatNumber<SubDigit<N, [1]>, []>
->
-
-type RootNInverse<N extends Digit[]> = SignFloatNumber<"+", DivideInt<[1], N>>
-
-// Newton's method
-type RootStepNext<
-  Alpha extends SignFloatNumber,
-  NInverse extends SignFloatNumber,
-  NSubOne extends SignFloatNumber,
-  Step extends SignFloatNumber
-> = PowerSignFloatNumbers<
-  Step,
-  NSubOne
-> extends infer StepPowNSubOne extends SignFloatNumber
-  ? DivideSignFloatNumber<
-      Alpha,
-      StepPowNSubOne
-    > extends infer RightAdd extends SignFloatNumber
-    ? MultiplySignFloat<
-        NSubOne,
-        Step
-      > extends infer LeftAdd extends SignFloatNumber
-      ? AddSignFloatNumber<
-          LeftAdd,
-          RightAdd
-        > extends infer Bracket extends SignFloatNumber
-        ? MultiplySignFloat<NInverse, Bracket>
-        : never
-      : never
-    : never
-  : never
-
-type RootDigit<
-  Alpha extends SignFloatNumber,
-  N extends Digit[],
-  Step extends SignFloatNumber = SignFloatNumber<"+", FloatNumber<[1], []>>,
-  StepCnt extends Array<0> = []
-> = StepCnt["length"] extends 7
-  ? Step
-  : RootNInverse<N> extends infer NInverse extends SignFloatNumber
-  ? RootNSubOne<N> extends infer NSubOne extends SignFloatNumber
-    ? RootStepNext<
-        Alpha,
-        NInverse,
-        NSubOne,
-        Step
-      > extends infer NextStep extends SignFloatNumber
-      ? RootDigit<Alpha, N, NextStep, [...StepCnt, 0]>
-      : never
-    : never
-  : never
 
 type RootDigitIter<
   NSubOne extends SignFloatNumber,
@@ -96,7 +41,7 @@ type RootDigitIter<
     : never
   : never
 
-type RootDigit2<
+type RootDigit<
   Alpha extends SignFloatNumber,
   N extends Digit[]
 > = SignFloatNumber<
@@ -120,6 +65,6 @@ export type Root<
   N extends NumberLike
 > = ParseSignFloatNumber<X> extends infer XNumber extends SignFloatNumber
   ? ParseSignFloatNumber<N> extends infer Root extends SignFloatNumber
-    ? StringifySignFloat<RootDigit2<XNumber, Root["float"]["int"]>>
+    ? StringifySignFloat<RootDigit<XNumber, Root["float"]["int"]>>
     : never
   : never
